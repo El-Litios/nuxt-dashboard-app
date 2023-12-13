cef64f96066b7fe3559c03b5b821e557f6ec8aff
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-13 16:07:17 -0300

    add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "nuxt-headlessui",
+    "@morev/vue-transitions/nuxt",
+    '@vueuse/nuxt'
+  ],
+  tailwindcss: {exposeConfig: true},
+  headlessui: { prefix: 'H' },
+  app: {
+    head: {
+      title: 'Analytics',
+      link: [
+        {rel: 'icon', type: 'image/x-icon', href: '/icon.svg'},
+        {rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css'},
+        {rel: 'stylesheet', href: 'https://rsms.me/'}
+
+      ]
+    }
+  }
 })
