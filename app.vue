8a65affab0ab8f7e9a4614f3c64ec149eaa511f2
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-13 16:07:08 -0300

    added distribution to app

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- Header -->
+    <Header></Header>
+    <div>
+      <!-- Sidebar -->
+
+      <Sidebar></Sidebar>
+      <!-- Main section -->
+      <main>
+
+
+        <!-- Status cards -->
+
+
+        <!-- Costumer List -->
+
+
+        <!-- Sales Charts -->
+
+
+        <!-- Recent orders -->
+      </main>
+
+      <!-- Profile info section -->
+
+
+      <!-- Reminders -->
+    </div>
   </div>
 </template>
