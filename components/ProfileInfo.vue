935c069e79bdf155f17e7125e4417b72f435ce11
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-20 16:05:44 -0300

    added profile info component

---
+++ components/ProfileInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=" p-4 border bg-background">
+    <img
+      class="mx-auto h-28 w-28 rounded-full ring-1 ring-border"
+      src="https://randomuser.me/api/portraits/med/men/75.jpg"
+      alt="Logged in user"
+    />
+    <div class="mt-5 text-center">
+      <h3 class="font-medium">John Doe</h3>
+      <p class="mt-1 text-sm text-muted-foreground">
+        <a href="mailto:johndoe@gmail.com">johndoe@gmail.com</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
