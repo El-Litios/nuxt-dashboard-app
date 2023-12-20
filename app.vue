3f3c3612f53236477be614e60f906b9b19de7aa9
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-20 16:05:09 -0300

    imported components

--- app.vue
+++ app.vue
@@ -2,30 +2,32 @@
   <div>
     <!-- Header -->
     <Header></Header>
-    <div>
+    <div class="container lg:grid lg:grid-cols-12 lg:gap-10">
       <!-- Sidebar -->
 
-      <Sidebar></Sidebar>
+      <Sidebar class="hideen lg:block lg:col-span-3"></Sidebar>
       <!-- Main section -->
-      <main>
-
-
+      <main class="py-5 lg:col-span-6">
         <!-- Status cards -->
-
+        <StatusCard></StatusCard>
 
         <!-- Costumer List -->
-
+        <Costumers></Costumers>
 
         <!-- Sales Charts -->
-
+        <SalesChart></SalesChart>
 
         <!-- Recent orders -->
+        <RecentOrders></RecentOrders>
       </main>
 
-      <!-- Profile info section -->
-
-
-      <!-- Reminders -->
+      <div class="hidden py-5 lg:col-span-3 lg:block">
+        <!-- Profile info section -->
+        <ProfileInfo></ProfileInfo>
+        
+        <!-- Reminders -->
+        <Reminders></Reminders>
+      </div>
     </div>
   </div>
 </template>
