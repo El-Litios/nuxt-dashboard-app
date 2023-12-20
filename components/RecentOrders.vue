4bc55b7e436158e8fbe616017f8fe048b56e4a6a
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-20 16:05:52 -0300

    added recent order table component

---
+++ components/RecentOrders.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-5">
+    <p class="font-bold">Recent orders</p>
+    <div class="mt-5 overflow-x-auto rounded-md bg-background scrollbar-thin scrollbar-thumb-input scrollbar-thumb-md">
+      <table class="w-full border-collapse">
+        <thead>
+          <tr class="border-b text-left text-xs">
+            <th class="p-4  font-medium uppercase text-muted-foreground">Products</th>
+            <th class="p-4  font-medium uppercase text-muted-foreground">Payments</th>
+            <th class="p-4  font-medium uppercase text-muted-foreground">Status</th>
+            <th class="p-4  font-medium uppercase text-muted-foreground"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="payment in 6" class="border-b text-left text-sm last:border-b-0 hover:bg-muted">
+            <td class="p-4">Nike Shoes</td>
+            <td class="p-4">Paid</td>
+            <td class="p-4">Shipped</td>
+            <td class="p-4">
+              <div class="flex items-center gap-3">
+                <button>
+                  <Icon name="heroicons:pencil" class="h-4 w-4 text-muted-foreground"></Icon>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
